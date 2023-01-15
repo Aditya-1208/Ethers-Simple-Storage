74618e84a1263c248e6629613cf62424976637d9
Aditya <adityaagr.201ec103@gmail.com>
2023-01-15 16:27:38 +0530

    deploying contracts

---
+++ deploy.js
@@ -0,0 +1,51 @@
+const ethers = require("ethers");
+const fs = require("fs-extra");
+require("dotenv").config();
+
+async function main() {
+  const provider = new ethers.providers.JsonRpcProvider(process.env.RPC_URL);
+  const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
+  const abi = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.abi", "utf-8");
+  const binary = fs.readFileSync(
+    "./SimpleStorage_sol_SimpleStorage.bin",
+    "utf-8"
+  );
+  const contractFactory = new ethers.ContractFactory(abi, binary, wallet);
+  console.log("Starting to deploy a contract :)");
+  const contract = await contractFactory.deploy();
+  const transactionReceipt = await contract.deployTransaction.wait(1);
+
+  // nonce = await wallet.getTransactionCount();
+
+  // const tx = {
+  //   nonce,
+  //   gasPrice: 2000000000,
+  //   gasLimit: 1000000,
+  //   to: null,
+  //   value: 0,
+  //   data: "0x" + binary,
+  //   chainId: 1337,
+  // };
+
+  // const sentTxResponse = await wallet.sendTransaction(tx);
+  // await sentTxResponse.wait(1);
+  // console.log(sentTxResponse);
+
+  console.log(
+    `current favourite number : ${(await contract.retrieve()).toString()}`
+  );
+
+  const transactionResponse = await contract.store("13");
+  console.log(
+    `Updated favourite number : ${(await contract.retrieve()).toString()}`
+  );
+  // return transactionReceipt;
+}
+
+main()
+  .then((res) => {
+    console.log(res);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
